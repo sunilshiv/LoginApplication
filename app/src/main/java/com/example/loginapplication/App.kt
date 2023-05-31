3006613c3f65fb1c2ed122e8111abe0fb77fdab3
Sunil D Shivanakkanavar <sunil.d.shiv@gmail.com>
2023-05-31 12:50:18 +0530

    Add login usecase and functionality

--- app/src/main/java/com/example/loginapplication/App.kt
+++ app/src/main/java/com/example/loginapplication/App.kt
@@ -2,7 +2,16 @@ package com.example.loginapplication
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber.*
+import timber.log.Timber.Forest.plant
 
 @HiltAndroidApp
 class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            plant(DebugTree())
+        }
+    }
 }
